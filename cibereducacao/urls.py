aa401f8073fa57f2caf2e14ee03c1aa1ecb91056
Gabriel Rolim <gabrielsrolim@gmail.com>
2011-06-25 20:47:20 -0300

    Inicio do Projeto. Brincando do Django.

---
+++ cibereducacao/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+from cibereducacao.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    #('^$', my_homepage_view),
+    (r'^hello/$',hello),
+    (r'^time/$', current_datetime),
+    (r'^time/plus/(\d{1,2})/$', hours_ahead),
+    # Examples:
+    # url(r'^$', 'cibereducacao.views.home', name='home'),
+    # url(r'^cibereducacao/', include('cibereducacao.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
