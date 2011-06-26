b5eecb5ff1d67f97b1a36f46e03e6bff3468fb63
Gabriel Rolim <gabrielsrolim@gmail.com>
2011-06-26 02:02:22 -0300

    Terminado a parte da brincadeira com templates. Começamos agora a mexer com modelos de dados e "apps"

--- cibereducacao/settings.py
+++ cibereducacao/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'books',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'postgres',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -103,6 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'cibereducacao.urls'
 
 TEMPLATE_DIRS = (
+    '/home/gabriel/proinfo/sitepriscila/cibereducacao/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- cibereducacao/views.py
+++ cibereducacao/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import datetime
 
@@ -5,15 +6,14 @@ def hello(request):
     return HttpResponse("Hello world")
 
 def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body> It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    current_date = datetime.datetime.now()
+    #now = datetime.datetime.now()
+    return render_to_response('current_datetime.html',locals())
 
 def hours_ahead(request,offset):
     try:
-        offset = int(offset)
+        hour_offset = int(offset)
     except ValueError:
         raise Http404()
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    next_time = datetime.datetime.now() + datetime.timedelta(hours=hour_offset)
+    return render_to_response('hours_ahead.html',locals())
